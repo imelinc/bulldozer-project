a3da8d79f93c263bf9133cd5debf37b1704f37f8
imelinc <ignaciomelinc@gmail.com>
2025-06-07 18:20:37 -0300

    Started on the .py file

---
+++ bulldozer.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns 
+import matplotlib.pyplot as plt
+
+df= pd.read_csv("../data/bluebook-for-bulldozers/TrainAndValid.csv", low_memory=False)

8b027637b37a70d3cdfa5fa6715e7390a3bb3684
imelinc <ignaciomelinc@gmail.com>
2025-06-07 19:58:31 -0300

    handled the missing values

--- bulldozer.py
+++ bulldozer.py
@@ -19,5 +19,14 @@ df["saleDayOfYear"] = df.saledate.dt.dayofyear
 # And we drop the saledate column
 df = df.drop("saledate", axis = 1)
 
-# Then, we're going to turn everything into numeric values so the model can understand it
+# Then, we're going to handle the missing values
+for col in df.columns:
+    if df[col].isna().sum() > 330158: # 80% of the data is missing
+        df = df.drop(col, axis=1)
+    else:
+        if df[col].dtype == "object":
+            df[col] = df[col].fillna("missing")
+        else:
+            df[col] = df[col].fillna(df[col].median())
+            
 

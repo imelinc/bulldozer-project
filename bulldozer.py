dca3c756291daf20724d1b5eecfb3e69d379cdc6
imelinc <ignaciomelinc@gmail.com>
2025-06-08 21:19:25 -0300

    Turned into categorical values

--- bulldozer.py
+++ bulldozer.py
@@ -11,4 +11,12 @@ df['saleDay'] = df.saledate.dt.day
 df['saleDayOfWeek'] = df.saledate.dt.dayofweek
 df['saleDayOfYear'] = df.saledate.dt.dayofyear
 
-# 
+# Now we can drop the saledate column
+df = df.drop("saledate", axis=1)
+
+# Now we're ready to turn everything into numbers, and that is goint to be done by using the pandas "categorical" type
+
+for label, content in df.items():
+    if pd.api.types.is_object_dtype(content): # if the column is a 'object' data type
+        df[label] = content.astype("category").cat.as_ordered() # convert to categorical type
+

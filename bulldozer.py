7e8e49548683970007cb1848de0c6cf92bb8bf4b
imelinc <ignaciomelinc@gmail.com>
2025-06-08 21:10:06 -0300

    Added features

--- bulldozer.py
+++ bulldozer.py
@@ -1,3 +1,14 @@
 import pandas as pd
 
 df = pd.read_csv("../data/bluebook-for-bulldozers/TrainAndValid.csv", low_memory = False, parse_dates=["saledate"])
+
+# We have plenty of missing value sin this dataset, we're goint to handle those in a moment
+# First, let's add some features that could be useful for our model
+
+df['saleYear'] = df.saledate.dt.year
+df['saleMonth'] = df.saledate.dt.month
+df['saleDay'] = df.saledate.dt.day
+df['saleDayOfWeek'] = df.saledate.dt.dayofweek
+df['saleDayOfYear'] = df.saledate.dt.dayofyear
+
+# 
